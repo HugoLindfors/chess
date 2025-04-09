8e690f34d261e7d6ed2a661f15aa71db1e5ebd4b
HugoLindfors <hugo.lindfors@outlook.com>
2025-04-09 22:41:18 +0200

    updated pieces with classes & ids

--- src/main.js
+++ src/main.js
@@ -27,29 +27,29 @@ function renderChessboard() {
 }
 
 function renderWhiteQRPawn() {
-  document.querySelector(`#tile-1-slot`).innerHTML += `<div class="piece piece-black">R</div>`
-  document.querySelector(`#tile-2-slot`).innerHTML += `<div class="piece piece-black">N</div>`
-  document.querySelector(`#tile-3-slot`).innerHTML += `<div class="piece piece-black">B</div>`
-  document.querySelector(`#tile-4-slot`).innerHTML += `<div class="piece piece-black">Q</div>`
-  document.querySelector(`#tile-5-slot`).innerHTML += `<div class="piece piece-black">K</div>`
-  document.querySelector(`#tile-6-slot`).innerHTML += `<div class="piece piece-black">B</div>`
-  document.querySelector(`#tile-7-slot`).innerHTML += `<div class="piece piece-black">N</div>`
-  document.querySelector(`#tile-8-slot`).innerHTML += `<div class="piece piece-black">R</div>`
+  document.querySelector(`#tile-1-slot`).innerHTML += `<div id="QR-black" class="rook rook-black piece piece-black">R</div>`
+  document.querySelector(`#tile-2-slot`).innerHTML += `<div id="QN-black" class="knight knight-black piece piece-black">N</div>`
+  document.querySelector(`#tile-3-slot`).innerHTML += `<div id="QB-black" class="bishop bishop-black piece piece-black">B</div>`
+  document.querySelector(`#tile-4-slot`).innerHTML += `<div id="Q-black" class="queen queen-black piece piece-black">Q</div>`
+  document.querySelector(`#tile-5-slot`).innerHTML += `<div id="K-black" class="king king-black piece piece-black">K</div>`
+  document.querySelector(`#tile-6-slot`).innerHTML += `<div id="QB-black" class="bishop bishop-black piece piece-black">B</div>`
+  document.querySelector(`#tile-7-slot`).innerHTML += `<div id="QN-black" class="knight knight-black piece piece-black">N</div>`
+  document.querySelector(`#tile-8-slot`).innerHTML += `<div id="QR-black" class="rook rook-black piece piece-black">R</div>`
 
   for(let i = 9; i <= 16; i++) {
-    document.querySelector(`#tile-${i}-slot`).innerHTML += `<div class="pawn piece-black"></div>`
+    document.querySelector(`#tile-${i}-slot`).innerHTML += `<div class="pawn piece piece-black"></div>`
   }
 
   for(let i = 49; i <= 56; i++) {
-    document.querySelector(`#tile-${i}-slot`).innerHTML += `<div class="pawn piece-white"></div>`
+    document.querySelector(`#tile-${i}-slot`).innerHTML += `<div class="pawn piece piece-white"></div>`
   }
 
-  document.querySelector(`#tile-57-slot`).innerHTML += `<div class="piece piece-white">R</div>`
-  document.querySelector(`#tile-58-slot`).innerHTML += `<div class="piece piece-white">N</div>`
-  document.querySelector(`#tile-59-slot`).innerHTML += `<div class="piece piece-white">B</div>`
-  document.querySelector(`#tile-60-slot`).innerHTML += `<div class="piece piece-white">Q</div>`
-  document.querySelector(`#tile-61-slot`).innerHTML += `<div class="piece piece-white">K</div>`
-  document.querySelector(`#tile-62-slot`).innerHTML += `<div class="piece piece-white">B</div>`
-  document.querySelector(`#tile-63-slot`).innerHTML += `<div class="piece piece-white">N</div>`
-  document.querySelector(`#tile-64-slot`).innerHTML += `<div class="piece piece-white">R</div>`
+  document.querySelector(`#tile-57-slot`).innerHTML += `<div id="QR-white" class="rook rook-white piece piece-white">R</div>`
+  document.querySelector(`#tile-58-slot`).innerHTML += `<div id="QN-white" class="knight knight-white piece piece-white">N</div>`
+  document.querySelector(`#tile-59-slot`).innerHTML += `<div id="QB-white" class="bishop bishop-white piece piece-white">B</div>`
+  document.querySelector(`#tile-60-slot`).innerHTML += `<div id="Q-white" class="queen queen-white piece piece-white">Q</div>`
+  document.querySelector(`#tile-61-slot`).innerHTML += `<div id="K-white" class="king king-white piece piece-white">K</div>`
+  document.querySelector(`#tile-62-slot`).innerHTML += `<div id="QB-white" class="bishop bishop-white piece piece-white">B</div>`
+  document.querySelector(`#tile-63-slot`).innerHTML += `<div id="QN-white" class="knight knight-white piece piece-white">N</div>`
+  document.querySelector(`#tile-64-slot`).innerHTML += `<div id="QR-white" class="rook rook-white piece piece-white">R</div>`
 }
